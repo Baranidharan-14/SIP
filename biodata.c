c7cfaebbe6a0eb4df8886da9490aaad14d6f9a69
Baranidharan-14 <baranidharan.1420@gmail.com>
2024-09-19 15:52:49 +0530

    BIODATA.c

---
+++ biodata.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name            :Baranidharan.R\n");
+    printf("Father name     :Rajendiran.C\n");
+    printf("Mother name     :Ramani.R\n");
+    printf("Fater occupation:Milk secreatary\n");
+    printf("Motheroccupation:Housewife\n");
+    printf("Nationality     :Hindhu\n");
+    printf("Hobby           :Listening music,watching cricket\n");
+    printf("Adress          :133/1APPINAYAKANPATTI(vil)\n");
+                             ("UTHANGARAI(PO)\n");
+                             ("KRISHNAGIRI(DT)-635207\n");
+    printf("Mobile no       :7812811382\n");
+    printf("Aim             :To become a software engineer\n");
+    
+    
+    return 0;
+}
